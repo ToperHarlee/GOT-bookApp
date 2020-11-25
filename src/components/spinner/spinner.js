f25af266305627d95b510259af425979bcb6687b
Mikhail <epictapok@gmail.com>
2020-11-25 11:46:58 +0300

    получение данных из обьектов сервисав(типа апи), обработка ошибок, подключение спиннера ожидания

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-spinner-ewuugaucd7c"><div class="ldio-1885iotmf6t">
+            <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
--- src/services/gotService.js
+++ src/services/gotService.js
@@ -14,17 +14,51 @@ export default class GotService {
 
         return await res.json();
     };
-    getAllCharacters () {
-        return this.getResource('/characters?page=5&pageSize=10');
+    async getAllCharacters () {
+        const res = await this.getResource('/characters?page=5&pageSize=10');
+        return res.map(this._transformCharacter);
     }
-    getCharacter(id) {
-        return this.getResource(`/characters/${id}`);
+    async getCharacter(id) {
+        const character = await this.getResource(`/characters/${id}`);
+        return this._transformCharacter(character);
     }
-    getAllHouses() {
-        return this.getResource('/houses');
+    async getAllHouses() {
+        const houses = await this.getResource('/houses');
+        return houses.map(this._transformCharacter)
     }
-    getHouse(id) {
-        return this.getResource(`/houses/${id}`);
+    async getHouse(id) {
+        const house = await this.getResource(`/houses/${id}`);
+        return this._transformCharacter(house);
+    }
+
+    _transformCharacter(char) {
+        return {
+            name: char.name,
+            gender: char.gender,
+            born: char.born,
+            died: char.died,
+            culture: char.culture
+        }
+    }
+
+    _transformHouse(house) {
+        return {
+            name: house.name,
+            region: house.region,
+            words: house.words,
+            titles: house.titles,
+            overlord: house.overlord,
+            ancestralWeapons: house.ancestralWeapons
+        }
+    }
+
+    _transformBook(book) {
+        return {
+            name: book.name,
+            numberOfPages: book.numberOfPages,
+            publisher: book.publisher,
+            released: book.released
+        }
     }
 }
 
